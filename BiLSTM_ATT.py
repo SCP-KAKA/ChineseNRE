df9670e861ca3729c5d044c55aa336b01b931b8d
李垚 <1074735496@qq.com>
2019-09-20 20:44:44 +0800

    代码语法错误修改
    在python3环境下，修改了Python2的错误。

--- BiLSTM_ATT.py
+++ BiLSTM_ATT.py
@@ -1,12 +1,13 @@
-#coding:utf8
+# coding:utf8
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 torch.manual_seed(1)
 
+
 class BiLSTM_ATT(nn.Module):
-    def __init__(self,config,embedding_pre):
-        super(BiLSTM_ATT,self).__init__()
+    def __init__(self, config, embedding_pre):
+        super(BiLSTM_ATT, self).__init__()
         self.batch = config['BATCH']
         
         self.embedding_size = config['EMBEDDING_SIZE']
@@ -20,26 +21,27 @@ class BiLSTM_ATT(nn.Module):
         
         self.pretrained = config['pretrained']
         if self.pretrained:
-            #self.word_embeds.weight.data.copy_(torch.from_numpy(embedding_pre))
+            # self.word_embeds.weight.data.copy_(torch.from_numpy(embedding_pre))
             self.word_embeds = nn.Embedding.from_pretrained(torch.FloatTensor(embedding_pre),freeze=False)
         else:
-            self.word_embeds = nn.Embedding(self.embedding_size,self.embedding_dim)
+            self.word_embeds = nn.Embedding(self.embedding_size, self.embedding_dim)
         
-        self.pos1_embeds = nn.Embedding(self.pos_size,self.pos_dim)
-        self.pos2_embeds = nn.Embedding(self.pos_size,self.pos_dim)
-        self.relation_embeds = nn.Embedding(self.tag_size,self.hidden_dim)
+        self.pos1_embeds = nn.Embedding(self.pos_size, self.pos_dim)
+        self.pos2_embeds = nn.Embedding(self.pos_size, self.pos_dim)
+        self.relation_embeds = nn.Embedding(self.tag_size, self.hidden_dim)
         
-        self.lstm = nn.LSTM(input_size=self.embedding_dim+self.pos_dim*2,hidden_size=self.hidden_dim//2,num_layers=1, bidirectional=True)
-        self.hidden2tag = nn.Linear(self.hidden_dim,self.tag_size)
+        self.lstm = nn.LSTM(input_size=self.embedding_dim+self.pos_dim*2,
+                            hidden_size=self.hidden_dim//2, num_layers=1, bidirectional=True)
+        self.hidden2tag = nn.Linear(self.hidden_dim, self.tag_size)
         
-        self.dropout_emb=nn.Dropout(p=0.5)
-        self.dropout_lstm=nn.Dropout(p=0.5)
-        self.dropout_att=nn.Dropout(p=0.5)
+        self.dropout_emb = nn.Dropout(p=0.5)
+        self.dropout_lstm = nn.Dropout(p=0.5)
+        self.dropout_att = nn.Dropout(p=0.5)
         
         self.hidden = self.init_hidden()
         
-        self.att_weight = nn.Parameter(torch.randn(self.batch,1,self.hidden_dim))
-        self.relation_bias = nn.Parameter(torch.randn(self.batch,self.tag_size,1))
+        self.att_weight = nn.Parameter(torch.randn(self.batch, 1, self.hidden_dim))
+        self.relation_bias = nn.Parameter(torch.randn(self.batch, self.tag_size, ))
         
     def init_hidden(self):
         return torch.randn(2, self.batch, self.hidden_dim // 2)
@@ -48,38 +50,34 @@ class BiLSTM_ATT(nn.Module):
         return (torch.randn(2, self.batch, self.hidden_dim // 2),
                 torch.randn(2, self.batch, self.hidden_dim // 2))
                 
-    def attention(self,H):
+    def attention(self, H):
         M = F.tanh(H)
-        a = F.softmax(torch.bmm(self.att_weight,M),2)
-        a = torch.transpose(a,1,2)
-        return torch.bmm(H,a)
-        
-    
-                
-    def forward(self,sentence,pos1,pos2):
+        a = F.softmax(torch.bmm(self.att_weight, M), 2)
+        a = torch.transpose(a, 1,)
+        return torch.bmm(H, a)
 
+    def forward(self, sentence, pos1, pos2):
         self.hidden = self.init_hidden_lstm()
 
-        embeds = torch.cat((self.word_embeds(sentence),self.pos1_embeds(pos1),self.pos2_embeds(pos2)),2)
+        embeds = torch.cat((self.word_embeds(sentence), self.pos1_embeds(pos1), self.pos2_embeds(pos2)), 2)
         
-        embeds = torch.transpose(embeds,0,1)
+        embeds = torch.transpose(embeds, 0, 1)
 
         lstm_out, self.hidden = self.lstm(embeds, self.hidden)
         
-        lstm_out = torch.transpose(lstm_out,0,1)
-        lstm_out = torch.transpose(lstm_out,1,2)
+        lstm_out = torch.transpose(lstm_out, 0, 1)
+        lstm_out = torch.transpose(lstm_out, 1, 2)
         
         lstm_out = self.dropout_lstm(lstm_out)
         att_out = F.tanh(self.attention(lstm_out))
-        #att_out = self.dropout_att(att_out)
+        # att_out = self.dropout_att(att_out)
         
-        relation = torch.tensor([i for i in range(self.tag_size)],dtype = torch.long).repeat(self.batch, 1)
+        relation = torch.tensor([i for i in range(self.tag_size)], dtype=torch.long).repeat(self.batch, 1)
 
         relation = self.relation_embeds(relation)
         
-        res = torch.add(torch.bmm(relation,att_out),self.relation_bias)
+        res = torch.add(torch.bmm(relation, att_out), self.relation_bias)
         
-        res = F.softmax(res,1)
+        res = F.softmax(res, 1)
 
-        
-        return res.view(self.batch,-1)
+        return res.view(self.batch, -1)
